8e2051be251db06687b57b10cbc1b763a1c5e66c
nsedrickm <nsedrick101@gmail.com>
2019-11-17 22:13:39 +0100

    addToHome

--- app-script.js
+++ app-script.js
@@ -1,11 +1,13 @@
 var content = document.getElementById("content");
 var button = document.getElementById("continue-button");
 var subButton = document.getElementById("Button_Image");
+var addToHome = document.getElementById("addToHome");
 
 button.addEventListener("click", () => {
   content.innerHTML = `
 
       <div class=" col-sm-12 col-md-12 my-5">
+
         <form id="formmomo"
           method="GET"
           action="https://developer.mtn.cm/OnlineMomoWeb/faces/transaction/transactionRequest.xhtml">
@@ -21,33 +23,54 @@ button.addEventListener("click", () => {
           <button type="image" id="Button_Image" name="submit" alt="OnloneMomo, le réflexe sécurité pour payer en ligne" autocomplete="off" class="btn btn-lg btn-warning btn-default rounded-pill text-white px-5 py-3 m-5 font-weight-bold">
             Get Bonus
           </button>
+           <button
+      id="addToHome"
+      class="btn btn-outline-warning rounded-pill text-white px-5 py-3 font-weight-bold lato"
+    >
+      add to home screen
+    </button>
         </form>
         </div>
 
 `;
 });
 
-let deferredPrompt;
-
 /* prompt user to  add to their home screen */
+let promptEvt;
 
 window.addEventListener("beforeinstallprompt", evt => {
   evt.preventDefault();
+  console.log(evt);
 
   //save event for future use
   promptEvt = evt;
 
+  // Update UI notify the user they can add to home screen
+  addToHome.style.display = "block";
+
   return false;
 });
 
-if (promptEvt !== undefined) {
+function checkInstalledState() {
+  console.log("subButton clicked ");
   //show install banner now
-  promptEvt.prompt();
+  if (promptEvt !== undefined) {
+    // hide our user interface that shows our A2HS button
+    addToHome.style.display = "none";
 
-  promtEvt.userChoice.then(choice => {
-    console.log("User choice: ", choice.outcome);
-  });
+    promptEvt.prompt();
+
+    promptEvt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      promptEvt = null;
+    });
+  }
 }
+addToHome.addEventListener("click", checkInstalledState);
 
 //notifications
 
